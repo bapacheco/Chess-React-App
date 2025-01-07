15c478ad1f5ebb83486a05379b13e4fd5dd5afc1
Brandon Pacheco <BrandonAPacheco@Gmail.com>
2025-01-07 17:48:31 -0500

    Modify to work with docker - Added another localhost:8000 in SecurityConfig - Added conditional assignment in application properties

--- backend/spring_boot/src/main/java/com/bapachec/chess_api/config/SecurityConfig.java
+++ backend/spring_boot/src/main/java/com/bapachec/chess_api/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
