2743962b209016e45ea11fe46a73c44ca0693627
Brandon Pacheco <BrandonAPacheco@Gmail.com>
2025-02-02 13:49:38 -0500

    fixed memo bug

--- frontend/src/contexts/ApiProvider.js
+++ frontend/src/contexts/ApiProvider.js
@@ -5,7 +5,7 @@ const ApiContext = createContext();
 
 export default function ApiProvider({ children }) {
 
-    const api = useMemo(() => new ChessApiClient())
+    const api = useMemo(() => new ChessApiClient(), []);
 
     return (
         <ApiContext.Provider value={api}>
